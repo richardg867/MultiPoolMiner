112c6dae9aa787043c6baf7d1a47928907556ea0
RichardG867 <richardg867@gmail.com>
2018-03-06 19:03:04 -0300

    JayDDee 3.8.3.3

--- Miners/CpuminerJayddee.ps1
+++ Miners/CpuminerJayddee.ps1
@@ -28,7 +28,7 @@ If ($CpuInfo -match "<l3>(\d+) KB</l3>") {
     }
 }
 
-$Uri = "https://github.com/JayDDee/cpuminer-opt/files/1753108/cpuminer-opt-3.8.3.1-windows.zip"
+$Uri = "https://github.com/JayDDee/cpuminer-opt/files/1756603/cpuminer-opt-3.8.3.3-windows.zip"
 
 $Commands = [PSCustomObject]@{
     "allium" = "" #Garlicoin
