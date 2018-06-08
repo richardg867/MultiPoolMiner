dcbca829a72c7671bd611dde566b7f70086081dc
RichardG867 <richardg867@gmail.com>
2018-06-08 20:23:40 -0300

    Housekeeping part 3: Excavator AMD

--- Miners/ExcavatorAmd.ps1
+++ Miners/ExcavatorAmd.ps1
@@ -1,50 +1,58 @@
 ﻿using module ..\Include.psm1
 
-$Type = "AMD"
-if (-not $Devices.$Type -or $Config.InfoOnly) {return} # No AMD present in system
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject[]]$Devices
+)
 
-$Path = ".\Bin\Excavator\excavator.exe"
-$HashSHA256 = "4CC2FF8C07F17E940A1965B8D0F7DD8508096A4E4928704912FA96C442346642"
+$Path = ".\Bin\Excavator-AMD\excavator.exe"
+$HashSHA256 = "28D9298323080449BD71EFBAD9B7B2BB7BCCF139525A8410361B6953D57CDC65"
 $Uri = "https://github.com/nicehash/excavator/releases/download/v1.2.11a/excavator_v1.2.11a_Win64.zip"
+$Port = "5200"
 
-$Commands = [PSCustomObject]@{
-    "daggerhashimoto:1" = @() #Ethash 1 thread
-    "decred:1"          = @() #Decred 1 thread
-    "equihash:1"        = @() #Equihash 1 thread
-    "pascal:1"          = @() #Pascal 1 thread
-    "sia:1"             = @() #Sia 1 thread
-    "daggerhashimoto:2" = @() #Ethash 2 threads
-    "decred:2"          = @() #Decred 2 threads
-    "equihash:2"        = @() #Equihash 2 threads
-    "pascal:2"          = @() #Pascal 2 threads
-    "sia:2"             = @() #Sia 2 threads
-}
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{Algorithm = "daggerhashimoto"; Threads = 1; Params = @()} #Ethash
+    [PSCustomObject]@{Algorithm = "decred"; Threads = 1; Params = @()} #Decred
+    [PSCustomObject]@{Algorithm = "equihash"; Threads = 1; Params = @()} #Equihash
+    [PSCustomObject]@{Algorithm = "pascal"; Threads = 1; Params = @()} #Pascal
+    [PSCustomObject]@{Algorithm = "sia"; Threads = 1; Params = @()} #Siacoin
+    [PSCustomObject]@{Algorithm = "daggerhashimoto"; Threads = 2; Params = @()} #Ethash
+    [PSCustomObject]@{Algorithm = "decred"; Threads = 2; Params = @()} #Decred
+    [PSCustomObject]@{Algorithm = "equihash"; Threads = 2; Params = @()} #Equihash
+    [PSCustomObject]@{Algorithm = "pascal"; Threads = 2; Params = @()} #Pascal
+    [PSCustomObject]@{Algorithm = "sia"; Threads = 2; Params = @()} #Siacoin
+)
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
-$Port = 3456 + (0 * 10000)
+$Devices = $Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "Advanced Micro Devices, Inc."
 
-$DeviceIDs = @($Devices.$Type.DeviceIDs)
+$Devices | Select-Object -ExpandProperty Model | ForEach-Object {
+    $Miner_Device = $Devices | Where-Object Model -EQ $_
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
 
-$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {($DeviceIDs.Count -gt 0 -or $Config.InfoOnly) -and $_ -match ".+:[1-9]"} | ForEach-Object {
-    $Algorithm = $_.Split(":") | Select-Object -Index 0
-    $Algorithm_Norm = Get-Algorithm $Algorithm
+    $Commands | ForEach-Object {
+        $Algorithm = $_.Algorithm
+        $Algorithm_Norm = Get-Algorithm $Algorithm
+        $Threads = $_.Threads
+        $Params = $_.Params
+        $Miner_Name = (@($Name) + @($Threads) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
 
-    $Threads =  $_ -split ":" | Select -Index 1
-    $Miner_Name = "$($Name)$($Threads)"
-
-    if ($Pools.$Algorithm_Norm.Host) {
-        [PSCustomObject]@{
-            Name             = $Miner_Name
-            Type             = "AMD"
-            Path             = $Path
-            HashSHA256       = $HashSHA256
-            Arguments        = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Algorithm", "$([Net.DNS]::Resolve($Pools.$Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1):$($Pools.$Algorithm_Norm.Port)", "$($Pools.$Algorithm_Norm.User):$($Pools.$Algorithm_Norm.Pass)")}) + @([PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($DeviceIDs | ForEach-Object {@("alg-0", "$_")} | Select-Object) * $Threads) + $Commands.$_})
-            HashRates        = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
-            API              = "Excavator"
-            Port             = $Port
-            URI              = $Uri
-            PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
-            PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+        if ($Pools.$Algorithm_Norm.Host) {
+            [PSCustomObject]@{
+                Name             = $Miner_Name
+                DeviceName       = $Miner_Device.Name
+                Path             = $Path
+                HashSHA256       = $HashSHA256
+                Arguments        = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$Algorithm", "$([Net.DNS]::Resolve($Pools.$Algorithm_Norm.Host).AddressList.IPAddressToString | Select-Object -First 1):$($Pools.$Algorithm_Norm.Port)", "$($Pools.$Algorithm_Norm.User):$($Pools.$Algorithm_Norm.Pass)")}) + @([PSCustomObject]@{id = 1; method = "workers.add"; params = @(@($Miner_Device.Type_PlatformId_Index | ForEach-Object {@("alg-0", "$_")} | Select-Object) * $Threads) + $Params})
+                HashRates        = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API              = "Excavator"
+                Port             = $Miner_Port
+                URI              = $Uri
+                PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+                PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+            }
         }
     }
 }
